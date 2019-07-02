9d316b362fc534cfbab8f5dba4550359a82e33a4
wangyiyun <7k10k@qq.com>
2019-07-02 16:05:07 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Loadable from 'react-loadable';
+
+const Wode = Loadable({
+    // 需要异步加载的组件
+    loader: () => import('./views/Wode'),
+    // 加载中的提示组件
+    loading: () => <div>加载中</div>
+  });
+  
+  export default class App extends Component {
+    render() {
+      return (
+        <Router>
+          <Switch>
+            <Route path="/wode" component={Wode} />
+            {/* <Route path="/sign" component={Sign} />
+            <AuthRoute path="/" component={Home} /> */}
+          </Switch>
+        </Router>
+      );
+    }
+  }
